52fd6d370a080435db4a0a581492adcf8710ec1b
matinebrahimi738-lang <matinebrahimi738@gmail.com>
2026-07-27 07:22:27 +0330

    Add clear button and analysis time v1.0.1.3

--- app.js
+++ app.js
@@ -6,11 +6,12 @@ let price = document.getElementById("price").value;
 
 let result = document.getElementById("result");
 
+let time = document.getElementById("time");
+
 
 if(coin==""){
 
 result.innerHTML="❌ نام ارز را وارد کن";
-
 return;
 
 }
@@ -19,34 +20,65 @@ return;
 if(price==""){
 
 result.innerHTML="❌ قیمت ارز را وارد کن";
-
 return;
 
 }
 
 
+let score;
+
 
 if(price < 100){
 
 result.innerHTML =
-"🟢 "+coin+"<br><br>قیمت پایین است<br>برای بررسی خرید مناسب‌تر است";
+"🟢 "+coin+"<br><br>شرایط مثبت<br>مناسب برای بررسی خرید";
+
+score = "8/10";
 
 }
 
 else if(price < 1000){
 
 result.innerHTML =
-"🟡 "+coin+"<br><br>نیاز به بررسی بیشتر دارد";
+"🟡 "+coin+"<br><br>نیاز به بررسی بیشتر";
+
+score = "5/10";
 
 }
 
 else{
 
 result.innerHTML =
-"🔴 "+coin+"<br><br>قیمت بالا است<br>فعلاً صبر کن";
+"🔴 "+coin+"<br><br>ریسک بالا، فعلاً صبر کن";
+
+score = "3/10";
 
 }
 
 
+result.innerHTML +=
+"<br><br>⭐ امتیاز بازار: "+score;
+
+
+let now = new Date();
+
+time.innerHTML =
+"⏰ آخرین تحلیل: " + now.toLocaleString("fa-IR");
+
+
 }
 
+
+
+function clearData(){
+
+document.getElementById("coin").value="";
+
+document.getElementById("price").value="";
+
+document.getElementById("result").innerHTML=
+"نتیجه تحلیل اینجا نمایش داده می‌شود";
+
+document.getElementById("time").innerHTML="";
+
+}
